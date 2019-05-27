9984429c9d688f1cbb3287a63550f3fe202e229b
sacachi <sacachi.mr1@gmail.com>
2019-05-27 15:03:34 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    db: {
+        host: 'localhost',
+        port: '5432',
+        database: 'quoccuong',
+        username: 'quoccuong',
+        password: '',
+        dialect: 'postgres',
+        logging: false
+    },
+    pagination: 5
+}
